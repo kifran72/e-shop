121ca5759c6937232c85b481d2ff7c370b630794
Kifran <cr91700kifran@gmail.com>
2022-03-09 21:48:22 +0100

    Hosting ready

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,65 @@
 </template>
 <script>
 import { defineComponent } from "vue";
+import { useMeta } from 'quasar'
+
+
+const metaData = {
+  // sets document title
+  title: 'E-shop',
+  // optional; sets final title as "Index Page - My Website", useful for multiple level meta
+  titleTemplate: title => `${title} - Fiters`,
+
+  // meta tags
+  meta: {
+    description: { name: 'description', content: 'Accueil' },
+    keywords: { name: 'keywords', content: 'e-shop' },
+    equiv: { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
+    // note: for Open Graph type metadata you will need to use SSR, to ensure page is rendered by the server
+    ogTitle: {
+      property: 'og:title',
+      // optional; similar to titleTemplate, but allows templating with other meta properties
+      template(ogTitle) {
+        return `${ogTitle} - E-shop`
+      }
+    }
+  },
+
+  // CSS tags
+  link: {
+    material: { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+  },
+
+  // JS tags
+  script: {
+    ldJson: {
+      type: 'application/ld+json',
+      innerHTML: `{ "@context": "http://schema.org" }`
+    }
+  },
+
+  // <html> attributes
+  htmlAttr: {
+    'xmlns:cc': 'http://creativecommons.org/ns#', // generates <html xmlns:cc="http://creativecommons.org/ns#">,
+    empty: undefined // generates <html empty>
+  },
+
+  // <body> attributes
+  bodyAttr: {
+    'action-scope': 'xyz', // generates <body action-scope="xyz">
+    empty: undefined // generates <body empty>
+  },
+
+  // <noscript> tags
+  noscript: {
+    default: 'This is content for browsers with no JS (or disabled JS)'
+  }
+}
 
 export default defineComponent({
   name: "App",
+  setup() {
+    useMeta(metaData)
+  }
 });
 </script>
